6ce896b5065274a2ce8a8682c83d58d3d33ce209
Christopherjfc <christopherjfc1@gmail.com>
2025-01-14 14:04:55 +0100

    Penúltimos cambios: solo queda arreglar el bucle infinito cuando la potencaActual es igual a la potenciaObjectiu

--- 03-Sleep_i_Coet/sleepcoet/src/main/java/sleep/coet/Coet.java
+++ 03-Sleep_i_Coet/sleepcoet/src/main/java/sleep/coet/Coet.java
@@ -1,47 +1,58 @@
 package sleep.coet;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+
 public class Coet {
     public static void main(String[] args) {
-        
-        ArrayList<Motor> listaMotores = new ArrayList<>();
-        arranca(listaMotores);
-
+       
         try (BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in))) {
+            ArrayList<Motor> listaMotores = new ArrayList<>();
+            arranca(listaMotores, entrada);
+            
             while (true) {
-                System.out.print("Número: ");
-                int potObjectiu = Integer.parseInt(entrada.readLine()); // indica la potencia que se desea alcanzar
-                
+                int potObjectiu = Integer.parseInt(entrada.readLine()); // indica la potencia que se desea alcanzar por consola
                 if (passaAPotencia(potObjectiu)) {
-                    for (Motor motor : listaMotores) {
-                        motor.setPotencia(potObjectiu);
+                    listaMotores.get(0).setPotencia(potObjectiu);
+                    if (potObjectiu == 0) {
+                        break;
                     }
                 } else {
                     System.out.println("\nIntroduce un numero entre 0 y 10 (los 2 números incluidos)\n");
                 }
             }
-
         }catch(IOException ioe) {
             ioe.printStackTrace();
         }
     }
 
+
     public static boolean passaAPotencia(int potencia) {
         return potencia >=0 && potencia <= 10;
     }
 
-    public static void arranca(ArrayList<Motor> lista) {
+
+    public static void arranca(ArrayList<Motor> lista, BufferedReader entrada) throws IOException{
         for (int i = 0; i < 4; i++) {
             Motor motor = new Motor(i);
-            lista.add(motor);
-            motor.start();
+            if (motor.getPotenciaObjectiu() == 0) {
+                while (true) {
+                    int numero = Integer.parseInt(entrada.readLine());
+                    if (numero > 0 && numero <= 10) {
+                        motor.setPotencia(numero);
+                        lista.add(motor);
+                        motor.start();
+                        break;
+                    }
+                }
+            }else {
+                lista.add(motor);
+                motor.start();
+            }
         }
     }
-
 }
-
-
--- 03-Sleep_i_Coet/sleepcoet/src/main/java/sleep/coet/Motor.java
+++ 03-Sleep_i_Coet/sleepcoet/src/main/java/sleep/coet/Motor.java
@@ -1,36 +1,68 @@
 package sleep.coet;
 
-
 public class Motor extends Thread{
     private int potenciaActual = 0;
-    private int potenciaObjectiu = 0;
+    private static int potenciaObjectiu = 0;
     private int motor;
+    private boolean apagado = false;
+
 
     public Motor(int numero){
         motor = numero;
     }
 
+
     @Override
     public void run() {
         while (true) {
-            if (potenciaActual < potenciaObjectiu) {
-                potenciaActual++;
-                System.out.printf("Motor %d: %s. Objectiu: %d Actual: %d%n",motor, (potenciaActual == potenciaObjectiu) ? "ResFer" : "Incre", potenciaObjectiu, potenciaActual);
-            } else if (potenciaActual > potenciaObjectiu){
-                potenciaActual--;
-                System.out.printf("Motor %d: %s. Objectiu: %d Actual: %d%n",motor, (potenciaActual == potenciaObjectiu) ? "ResFer" : "Decre", potenciaObjectiu, potenciaActual);
-            } else {
-                System.out.printf("Motor %d: ResFer. Objectiu: %d Actual: %d%n", motor, potenciaObjectiu, potenciaActual);
+            while (potenciaObjectiu != 0) {
+                if (potenciaActual < potenciaObjectiu) {
+                    potenciaActual++;
+                    System.out.printf("Motor %d: %s. Objectiu: %d Actual: %d%n",motor, (potenciaActual == potenciaObjectiu) ? "ResFer" : "Incre", potenciaObjectiu, potenciaActual);
+                } else if (potenciaActual > potenciaObjectiu){
+                    potenciaActual--;
+                    System.out.printf("Motor %d: %s. Objectiu: %d Actual: %d%n",motor, (potenciaActual == potenciaObjectiu) ? "ResFer" : "Decre", potenciaObjectiu, potenciaActual);
+                } else {
+                    System.out.println("Introdueix un nou numero: ");
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            if(potenciaActual >= 0 && potenciaActual >= 0){
+                if (potenciaActual != 0) potenciaActual--;
+                System.out.printf("Motor %d: %s. Objectiu: %d Actual: %d%n",motor, (potenciaActual == potenciaObjectiu) ? "ResFer" : "Decre", potenciaObjectiu, potenciaActual);
+                if (potenciaActual == 0){
+                    apagado = true;
+                    break;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
+
     public void setPotencia (int potencia) {
         potenciaObjectiu = potencia;
+        System.out.println("Passant a potència " + potenciaObjectiu);
+    }
+
+
+    public int getPotenciaObjectiu() {
+        return potenciaObjectiu;
+    }
+
+    public int getPotenciaActual() {
+        return potenciaActual;
+    }
+
+    public  boolean motoresApagados() {
+        return apagado;
     }
 }
